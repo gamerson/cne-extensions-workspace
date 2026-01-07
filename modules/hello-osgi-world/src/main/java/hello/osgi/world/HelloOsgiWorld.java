105d5b7b9247638d29837117268e275f1bdfa27a
Gregory Amerson <gregory.amerson@liferay.com>
2026-01-07 09:07:59 -0600

    init cne extensions workspace

---
+++ modules/hello-osgi-world/src/main/java/hello/osgi/world/HelloOsgiWorld.java
@@ -0,0 +1,16 @@
+package hello.osgi.world;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Activate;
+
+/**
+ * @author greg
+ */
+@Component(immediate = true)
+public class HelloOsgiWorld {
+
+    @Activate
+    public void activate() {
+        System.out.println("Hello Osgi World!");
+    }
+}
